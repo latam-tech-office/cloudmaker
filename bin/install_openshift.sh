2b65f87a64b4b54909f5cc455f2907645fb59e05
Mauricio Leal <maltron@gmail.com>
2017-03-25 07:08:40 -0300

    Several modifications in openshift

--- bin/install_openshift.sh
+++ bin/install_openshift.sh
@@ -1,1 +1,1 @@
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@example-master.example.com "ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no mauricio@master.example.com "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml"
