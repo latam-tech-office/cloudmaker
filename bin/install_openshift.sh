6d5afef6998d53d8ba7499137af0b6e5dce5e1c5
Mauricio Leal <maltron@gmail.com>
2017-03-26 17:21:16 -0300

    Added a post installation playbook

--- bin/install_openshift.sh
+++ bin/install_openshift.sh
@@ -1,1 +1,1 @@
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no mauricio@master.example.com "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@master.example.com "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml"
