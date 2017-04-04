a3d375f0f53a912d633cc0006f669657485b9ca8
Mauricio Leal <maltron@gmail.com>
2017-04-04 11:22:18 -0300

    Adjust infra /etc/hosts

--- bin/install_openshift.sh
+++ bin/install_openshift.sh
@@ -1,1 +1,1 @@
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@master.example.com "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@master.example.com "sudo ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml; ansible-playbook /root/post-install-openshift.yaml"
